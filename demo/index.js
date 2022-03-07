a1bfd5bcbf168b4721123879960b4fe08beafc6a
server <none>
2022-03-08 07:37:38 +0800

    demo changes on server

--- demo/index.js
+++ demo/index.js
@@ -17,23 +17,24 @@ app.use(express.static(path.join(__dirname)))
 
 
 app.get('/', function(req, res) {
-  db.mongo_find('users', {}, {projection: {_id: 0, username: 1, description: 1}}, 'demo-users.json').catch(console.dir);
-  let json_data = fs.readFileSync('demo-users.json');
-  let users_html = "";
-  let users = JSON.parse(json_data);
-  for(let x in users){
-    users_html += `<div class="demo-supporters-cells">
-        <h4> ${users[x].username} </h4>
-        <p> ${users[x].description} </p>
-    </div>`;
-  }
-  let demo_html = fs.readFileSync('demo-copy.html', 'utf-8');
-  let result = demo_html.replace(/<div class="demo-supporters">(.|\n)*?<\/div>/g, 
-    `<div class="demo-supporters">
-      ${users_html}
-    <\/div>\n`);
-  fs.writeFileSync('demo.html', result)
-  res.sendFile(path.join(__dirname, 'demo.html'))
+//   db.mongo_find('users', {}, {projection: {_id: 0, username: 1, description: 1}}, 'demo-users.json').catch(console.dir);
+//   let json_data = fs.readFileSync('demo-users.json');
+//   let users_html = "";
+//   let users = JSON.parse(json_data);
+//   for(let x in users){
+//     users_html += `<div class="demo-supporters-cells">
+//         <h4> ${users[x].username} </h4>
+//         <p> ${users[x].description} </p>
+//     </div>`;
+//   }
+//   let demo_html = fs.readFileSync('demo-copy.html', 'utf-8');
+//   let result = demo_html.replace(/<div class="demo-supporters">(.|\n)*?<\/div>/g, 
+//     `<div class="demo-supporters">
+//       ${users_html}
+//     <\/div>\n`);
+//   fs.writeFileSync('demo.html', result)
+//   res.sendFile(path.join(__dirname, 'demo.html'))
+res.sendFile(path.join(__dirname, 'homePage.html'));
 })
 
 app.get('/signup', function(req, res) {
@@ -41,8 +42,6 @@ app.get('/signup', function(req, res) {
 })
 
 app.post('/signup', function(req, res) {
-  console.log(req.body["name"])
-  console.log(req.body["bio"])
   db.mongo_insert('users', {
     username: req.body["name"], 
     address: "",
