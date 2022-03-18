e0b218a9007a9d0638057e27f79035546eb76cdd
hhqinging <hhqing2020@gmail.com>
2022-03-18 11:58:27 -0400

    upload forms

---
+++ react-frontend/src/Components/CreatForms.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Forms from "../Forms";
+const CreateForm = () => {
+  return (
+    <div>
+      <div>Create Your NFT track</div>
+      <Forms />
+      <button type="reset">Cancel</button>
+      <button type="submit">Create</button>
+    </div>
+  );
+};
+
+export default CreateForm;
