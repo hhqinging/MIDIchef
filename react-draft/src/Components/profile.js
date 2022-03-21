4da53640e4267f722599268d46d79c040124b53b
WX <xuewenhah@gmail.com>
2022-03-21 15:03:57 -0400

    compoent upadte

--- react-draft/src/Components/profile.js
+++ react-draft/src/Components/profile.js
@@ -1,25 +1,12 @@
 import * as React from "react";
 import ReactDOM from 'react-dom';
 import Button from '@mui/material/Button';
-import Paper from "@mui/material/Paper";
-import InputBase from "@mui/material/InputBase";
-import IconButton from "@mui/material/IconButton";
-import SearchIcon from '@mui/icons-material/Search';
-import Stack from '@mui/material/Stack';
-import FormGroup from '@mui/material/FormGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import './profile.css';
-import midicat from './midicat.png';
-
-
-function App() {
-  return <Button variant="contained">你好，世界</Button>;
-}
+import '../css/profile.css';
+import midicat from '../img/midicat.png'
 
 
 
-export default class Part4 extends React.Component {
+export default class Profile extends React.Component {
   render() {
     return (
       <div className="profile">
