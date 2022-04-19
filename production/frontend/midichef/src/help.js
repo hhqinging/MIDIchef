ef47ee9a478528800acd675602846a18251e3065
maihan <74171654+xll2385963328@users.noreply.github.com>
2022-04-19 18:47:29 -0400

    updated a simple test server to help test database

--- production/frontend/midichef/src/help.js
+++ production/frontend/midichef/src/help.js
@@ -12,3 +12,17 @@ export function post_helper(body,url='http://localhost:8000/api'){
       body: JSON.stringify(body) // body data type must match "Content-Type" header
     })
 }
+//post_helper
+//require input:
+//body：the form content you want to send to server.
+//url: optional, default is localhost:8000/api
+export function get_helper(body,url='http://localhost:8000/api'){
+    fetch(url, {
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      mode:'cors',
+      headers: {
+        'Content-Type': 'application/json; charset=UTF-8'
+      },
+      body: JSON.stringify(body) // body data type must match "Content-Type" header
+    })
+}
