6a5503484171acc259120afd996533dc913d6acb
WX <xuewenhah@gmail.com>
2022-04-23 18:53:25 -0400

    palce to change2

---
+++ production/frontend/midichef/placeToChange.js
@@ -0,0 +1,8 @@
+"D:\cse416\group\MIDIchef\archive\drafe2\my-app\src" -->
+mark with {/* Need t change */}
+
+
+Components\HomePlayer.js   
+Components\Profile.js     
+Components\UserPlayer.js
+(For now)
