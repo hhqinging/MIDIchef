ebda69f0873b4ea1af86910ff0dd1e7341a2d20e
maihan <74171654+xll2385963328@users.noreply.github.com>
2022-04-23 17:45:13 -0400

    updated data.js
    Also add the response to get action for user information and track information

--- production/backend/data.js
+++ production/backend/data.js
@@ -1,25 +1,48 @@
- export const WA = [
-    {
-    'WA': 'test'
-    },
-    {
-    'WA': 'test2'
-    }
-    ]
+ export const users = [
+   {  
+      'user': 'test',
+      'WalletA':'testwallet',
+      'picture':'/user/test.png',
+      'description':'My name is test',
+      'Followers': 0,
+      'Followering':0,
+      
+   },
+   {  
+      'user': 'test2',
+      'WalletA':'testwallet2',
+      'picture':'/user/test2.png',
+      'description':'My name is test2',
+      'Followers': 0,
+      'Followering' :0,
+   }
+   ]
  export const songs = [
     {
       'song': 'cat',
-      'uploader':'test',
+      'creater':'test',
+      'owner':'test2',
       'length':'13m',
-      'like':'0',
-      'price':'1'
-    },
+      'like':0,
+      'price':1,
+      'description':'This is a test song1' ,
+      'trans':{
+         'time':'2022-4-16',
+         'from':'test',
+         'to':'test2',
+         'price':1
+      }
+     },
     {
-    'song': 'happy',
-    'uploader':'test',
-    'length':'131m',
-    'like':'12',
-    'price':'7'
+      'song': 'happy',
+      'creater':'test',
+      'owner':'test',
+      'length':'131m',
+      'like':12,
+      'price':7,
+      'description':'This is a test song2',
+      'trans':{
+      }
     }
-    ]    
+]    
    
--- production/backend/server.js
+++ production/backend/server.js
@@ -1,6 +1,5 @@
 import {SignUp,JWT} from './library.js';
 import express from 'express';
-
 import cors from 'cors'
 import * as data from "./data.js";
 const app=express()
@@ -16,19 +15,20 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.post('/api/auth', (req,res) => {
     console.log(req.body)
-    let WA = data.WA.filter((WA) => {
-    return WA.WA == req.body.WA;
+    let user = data.users.filter((users) => {
+    return users.WalletA == req.body.WalletA;
     });
-    console.log(data.WA)
+    console.log(data.users)
     let token_payload = {
-        wallet: WA[0].WA
+        wallet: req.body.WalletA
         };
     console.log(token_payload)
     let token=JWT(token_payload)
-    if (WA.length){
+    if (user.length){
     // create a token using user name and password vaild for 2 hours
     let response = {
     message: 'Token Created, Authentication Successful!',
+    userName:user[0].user,
     token: token
     }; 
     // return the information including token as JSON
@@ -37,6 +37,7 @@ app.post('/api/auth', (req,res) => {
     SignUp();
     let response = {
     message: 'New User, Token Created, Authentication Successful!',
+    userName:'defult',
     token: token
     }; 
     return res.status(200).json(response);
@@ -59,22 +60,24 @@ app.post('/api/editProfile',(req,res)=>{
             }; 
         return res.status(409).json(response)
     }
-
 });
 app.get('/api/track',(req,res)=>{
     console.log(req.query)
     let song = data.songs.filter((songs) => {
-        return songs.song == req.query.song;
+        return songs.name == req.query.name;
         });
     console.log(song)
     if(song.length){
     let response = {
         message: 'song found!',
-        song: song[0].song,
-        uploader:song[0].uploader,
+        name: song[0].name,
+        creater:song[0].creater,
         length:song[0].length,
         likes:song[0].like,
-        price:song[0].price
+        price:song[0].price,
+        owner:song[0].owner,
+        trans:song[0].trans,
+        description:song[0].description
         }; 
     res.status(200).json(response);}
     else{
@@ -84,6 +87,56 @@ app.get('/api/track',(req,res)=>{
         return res.status(409).json(response)
     }
 })
+app.get('/api/GetUser',(req,res)=>{
+    let user = data.users.filter((users) => {
+        return users.user == req.query.user;
+        });
+    if (user.length){
+        // create a token using user name and password vaild for 2 hours
+        let response = {
+        message: 'User found!',
+        userName:user[0].user,
+        Followers:user[0].Followers,
+        description:user[0].description
+        }; 
+        // return the information including token as JSON
+        return res.status(200).json(response);
+        } else {
+        SignUp();
+        let response = {
+        message: 'User Not found',
+        }; 
+        return res.status(404).json(response);
+    }}); 
+
+// app.get('/api/search',(req,res)=>{
+//     let song = data.songs.filter((songs) => {
+//         return songs.name == req.query.song||songs.creater == req.query.creater
+//         });
+//     if (song.length){
+//         // create a token using user name and password vaild for 2 hours
+//         let response = {
+//          message: 'song found!',
+//          name: song[0].name,
+//          creater:song[0].creater,
+//          length:song[0].length,
+//          likes:song[0].like,
+//          price:song[0].price,
+//          owner:song[0].owner,
+//          trans:song[0].trans,
+//          description:song[0].description
+//          }; 
+//         // return the information including token as JSON
+//         return res.status(200).json(response);
+//         } else {
+//         SignUp();
+//         let response = {
+//         message: 'User Not found',
+//         }; 
+//         return res.status(404).json(response);
+//     }}); 
+    
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
   })
