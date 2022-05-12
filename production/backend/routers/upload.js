1b637903ccf5d45390e6e4f7a2a5adc7c4dfca61
WX <xuewenhah@gmail.com>
2022-05-12 15:58:55 -0400

    create nft page + upload backend to server folder

--- production/backend/routers/upload.js
+++ production/backend/routers/upload.js
@@ -1,5 +1,5 @@
-import express from 'express'
-import bodyParser from 'body-parser'
+import express from "express";
+import bodyParser from "body-parser";
 import multer from "multer";
 import fs from "fs";
 import fileUpload from "express-fileupload";
@@ -7,38 +7,51 @@ import cors from "cors";
 import morgan from "morgan";
 import _ from "lodash";
 
-var router = express.Router()
-router.use(fileUpload({
-    createParentPath: true
-}));
+var router = express.Router();
+router.use(
+    fileUpload({
+        createParentPath: true,
+    })
+);
 router.use(cors());
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: true }));
-router.use(morgan('dev'));
+router.use(morgan("dev"));
 
-router.post('/upload', async (req, res) => {
+router.post("/upload", async (req, res) => {
+    // console.log("req.files:", req.files)
+    // console.log("all:", req.body)
     try {
         if (!req.files) {
             res.send({
                 status: false,
-                message: 'No file uploaded'
+                message: "No file uploaded",
             });
         } else {
-            let file = req.files.file;
-            file.mv('./uploads/' + file.name);
-            res.json({name: file.name});
+            let music = req.files.music;
+            let imageCover = req.files.imageCover;
+            music.mv("./uploads/music/" + music.md5 + music.name);
+            imageCover.mv("./uploads/imageCover/" + imageCover.md5 + imageCover.name);
+            res.json({
+                title: req.body.title,
+                description: req.body.description,
+                price: req.body.price,
+                royalty: req.body.royalty,
+                music: "./uploads/music/" + music.name + music.md5,
+                imageCover: "./uploads/music/" + imageCover.name + imageCover.md5
+            });
         }
     } catch (err) {
         res.status(500).send(err);
     }
 });
 
-router.post('/upload-files', async (req, res) => {
+router.post("/upload-files", async (req, res) => {
     try {
         if (!req.files) {
             res.send({
                 status: false,
-                message: 'No file uploaded'
+                message: "No file uploaded",
             });
         } else {
             let data = [];
@@ -48,21 +61,21 @@ router.post('/upload-files', async (req, res) => {
                 let file = req.files.files[key];
 
                 //move photo to uploads directory
-                file.mv('./uploads/' + file.name);
+                file.mv("./uploads/" + file.name);
 
                 //push file details
                 data.push({
                     name: file.name,
                     mimetype: file.mimetype,
-                    size: file.size
+                    size: file.size,
                 });
             });
 
             //return response
             res.send({
                 status: true,
-                message: 'Files are uploaded',
-                data: data
+                message: "Files are uploaded",
+                data: data,
             });
         }
     } catch (err) {
@@ -70,9 +83,4 @@ router.post('/upload-files', async (req, res) => {
     }
 });
 
-
 export default router;
-
-
-
-
--- production/frontend/src/screens/CreateScreen.js
+++ production/frontend/src/screens/CreateScreen.js
@@ -5,6 +5,9 @@ import { useDispatch, useSelector } from "react-redux";
 import "../screens-css/CreateScreen.css";
 import styled from "styled-components";
 import Dropzone from "react-dropzone";
+import axios from 'axios';
+import React, { useState } from 'react';
+import { useDropzone } from "react-dropzone"
 
 const Container = styled.div`
   flex: 1;
@@ -77,65 +80,130 @@ const CreateScreen = () => {
   //   validationSchema: formSchema,
   // });
 
+  const initialValues = {
+    title: '',
+    description: '',
+    price: 0,
+    music: null,
+    royalty: 0
+  };
+
+  let [nft, setNft] = useState(initialValues);
+  const [imageCover, setImageCover] = useState([])
+
+  let onFileChange = (e) => {
+    e.preventDefault();
+    // console.log("e.target:", e.target.files)
+    setNft({
+      ...nft,
+      music: e.target.files[0]
+    })
+  }
+  let handleChange = (e) => {
+    const value = e.target.value;
+    setNft({
+      ...nft,
+      [e.target.name]: value
+    });
+  }
+  let onSubmit = (e) => {
+    e.preventDefault()
+    // console.log("nft:", nft)
+    // console.log("cover:", imageCover)
+    const formData = new FormData()
+    formData.append('music', nft.music)
+    formData.append('title', nft.title)
+    formData.append('description', nft.description)
+    formData.append('price', nft.price)
+    formData.append('royalty', nft.royalty)
+    formData.append('imageCover', imageCover[0])
+    axios.post("http://localhost:8000/api/upload", formData, {
+    }).then(res => {
+      console.log(res)
+    })
+  }
+
+  const { getRootProps, getInputProps } = useDropzone({
+    accept: "image/*",
+    onDrop: (acceptedFiles) => {
+      setImageCover(
+        acceptedFiles.map((file) =>
+          Object.assign(file, {
+            preview: URL.createObjectURL(file),
+          })
+        )
+      )
+    }
+  })
+
+  const images = imageCover.map((file) => (
+    <div key={file.name}>
+      <div>
+        <img src={file.preview} style={{ width: "200px" }} alt="preview" />
+      </div>
+    </div>
+  ))
+
   return (
     <div className="create">
       <h1 style={{ color: "white" }}>Create Your NFT Track</h1>
-      <form>
+      <form onSubmit={onSubmit}>
         <label>Title</label>
         <input
-          value={formik.values.title}
-          onChange={formik.handleChange("title")}
-          onBlur={formik.handleBlur("title")}
+          // value={formik.values.title}
+          // onChange={formik.handleChange("title")}
+          // onBlur={formik.handleBlur("title")}
           placeholder="NFT track title"
+          value={nft.title}
+          name="title"
+          onChange={handleChange}
         />
         <label>Description</label>
         <textarea
-          value={formik.values.description}
-          onChange={formik.handleChange("description")}
-          onBlur={formik.handleBlur("description")}
           placeholder="Brifly describe the NFT track"
+          value={nft.description}
+          name="description"
+          // onChange={(e) => setNft({ description: e.target.value })}
+          onChange={handleChange}
           style={{ height: "100px" }}
         />
         <label>Price</label>
         <input
-          value={formik.values.price}
-          onChange={formik.handleChange("price")}
-          onBlur={formik.handleBlur("price")}
           placeholder="price"
+          value={nft.price}
+          name="price"
+          // onChange={(e) => setNft({ price: e.target.value })}
+          onChange={handleChange}
         />
+
         <label>Image cover</label>
-        {/* <input type="file" placeholder="image" /> */}
-        <Container>
-          <Dropzone
-            onBlur={formik.handleBlur("image")}
-            accept="image/jpeg, image/png"
-            onDrop={(acceptedFiles) => {
-              formik.setFieldValue("image", acceptedFiles[0]);
-            }}
-          >
-            {({ getRootProps, getInputProps }) => (
-              <div className="container">
-                <div
-                  {...getRootProps({
-                    className: "dropzone",
-                    onDrop: (event) => event.stopPropagation(),
-                  })}
-                >
-                  <input {...getInputProps()} />
-                  <p style={{ color: "black" }}>Click here to select image</p>
-                </div>
-              </div>
-            )}
-          </Dropzone>
-        </Container>
+        <div {...getRootProps()}>
+          <input {...getInputProps()} />
+          <p style={{ color: "white" }}>Drop files here</p>
+        </div>
+        <div>{images}</div>
+
         <label>Track audio</label>
-        <input type="file" placeholder="Track audio" />
+        <input type="file"
+          placeholder="Track audio"
+          name="music"
+          // onChange={(e) => setNft({ music: e.target.files[0] })}
+          onChange={onFileChange}
+        />
         <label>Royalty</label>
-        <input placeholder="Royalty" />
+        <input placeholder="Royalty"
+          value={nft.royalty}
+          name="royalty"
+          // onChange={(e) => setNft({ royalty: e.target.value })} 
+          onChange={handleChange}
+        />
         <button>Cancel</button>
-        <button>Submit</button>
+        <button type="submit">Submit</button>
       </form>
     </div>
   );
 };
+
+
+
 export default CreateScreen;
