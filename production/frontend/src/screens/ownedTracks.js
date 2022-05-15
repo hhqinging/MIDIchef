0f9a9606e6b97224ad187e48719570181fefa503
QueenyLin <73852390+QueenyLin828@users.noreply.github.com>
2022-05-15 19:55:27 -0400

    cleaned up code a bit for owned tracks

--- production/frontend/src/screens/ownedTracks.js
+++ production/frontend/src/screens/ownedTracks.js
@@ -1,35 +1,22 @@
 import { useEffect, useReducer } from "react";
-import { useParams } from "react-router-dom";
 import axios from "axios";
 import logger from "use-reducer-logger";
 import TrackPlayer from "../screens-compo/TrackPlayer";
-// import data from "../data";
 import CircularProgress from "@mui/material/CircularProgress";
 import Grid from "@mui/material/Grid";
 import Card from "@mui/material/Card";
 import MessageBox from "../screens-compo/MessageBox";
-import { getError } from "../utils/utils";
-
 
 //taking two paras: current state & the action that changed current state and create the new state
 const reducer = (state, action) => {
   switch (action.type) {
     case "FETCH_REQUEST":
-      //it happens when sending axios req to backend
-      //...state: return newest state,
-      //keep prev state val and only update when loading: true
-      //loading :true, we can show loading to ui
       return { ...state, loading: true };
     case "FETCH_SUCCESS":
-      //keep prev state val, and only update tracks the data that coming from action, the data in action is in action.payload
-      //action.payload contains all tracks from backend,
-      //and we need to update loading to false since we success fetch data to frontend, no need tp show loading
       return { ...state, tracks: action.payload, loading: false };
     case "FETCH_FAIL":
-      //return prev state, and set loading to false[not show loading], and fail the error in the action.payload
       return { ...state, loading: false, error: action.payload };
     default:
-      //return current state
       return state;
   }
 };
@@ -37,35 +24,15 @@ const reducer = (state, action) => {
 function OwnedTracks() {
     let currentUser = localStorage.getItem("myalgo-wallet-addresses");
 
-    // const params = useParams();
-    // const { user } = params;
-    // console.log("user", currentUser);
-
     const [{ loading, error, tracks }, dispatch] = useReducer(logger(reducer), {
         tracks: [],
         loading: true,
         error: "",
     });
     console.log("TRACKS1", tracks);
-    // useEffect(() => {
-    //     const fetchData = async () => {
-    //       dispatch({ type: "FETCH_REQUEST" });
-    //       try {
-    //         // console.log("user2", currentUser);
-    //         const result = await axios.get(`/api/user/get_user?userName=${user}`);
-    //         dispatch({ type: "FETCH_SUCCESS", payload: result.data });
-    //       } catch (err) {
-    //         dispatch({ type: "FETCH_FAIL", payload: getError(err) });
-    //       }
-    //     };
-    //     fetchData();
-    //   }, []);
 
     useEffect(() => {
         const fetchData = async () => {
-          // const result = await axios.get("/api/tracks");
-          // setTracks(result.data);
-          //before send the axios, use dispatch to update the state, set loading to true
           dispatch({ type: "FETCH_REQUEST" });
           try {
             const result = await axios.get("/api/tracks/songlist");
@@ -82,23 +49,18 @@ function OwnedTracks() {
 
     const filteredTracks = tracks.filter((track) => track.owner === currentUser);
     console.log("TRACKS2", filteredTracks);
-    // tracks.filter(function(track) {
-    //     return track.owner === `${user}`
-    // });
 
     return (
         <div>
         <div className="tracks">
-            <Grid
-            container
-            style={{
-                display: "flex",
-                width: "92%",
-                margin: "auto",
-                justifyContent: "space-evenly",
-                spacing:"2"
-            }}
-            
+            <Grid container
+                style={{
+                    display: "flex",
+                    width: "92%",
+                    margin: "auto",
+                    justifyContent: "space-evenly",
+                    spacing:"2"
+                }}
             >
             {loading ? (
                 <CircularProgress
