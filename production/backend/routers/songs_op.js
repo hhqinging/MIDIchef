3d315a90db21dfda942d7b1e1c5da0d2abb57698
server <localhost>
2022-05-16 11:56:08 +0800

    upload.js

--- production/backend/routers/songs_op.js
+++ production/backend/routers/songs_op.js
@@ -2,7 +2,7 @@
 import express from 'express'
 import mongoose from 'mongoose';
 import Track from "../models/track-model.js";
-import { Create_song, Update_song } from './database_ops.js'
+import {Create_song,Update_song} from './database_ops.js'
 import path from 'path'
 //const and var
 var router = express.Router()
@@ -15,36 +15,36 @@ router.get('/songlist', function (req, res) {
         data = req.query[key];
     }
     Track.find()
-        .limit(20).then(test => {
-            if (test.length == 0) {
-                res.status(200).json({
-                    Info: "result not founded"
-                })
-            }
-            else {
-                res.status(200).json(
-                    test
-                )
-            }
-        })
+    .limit(60).then(test=>{
+        if(test.length==0){
+            res.status(200).json({
+                Info: "result not founded"
+            })
+        }
+        else{
+        console.log(test)
+        res.status(200).json(    
+            test
+        )}
+})
 })
 router.get('/trending', function (req, res) {
     for (var key in req.query) {
         data = req.query[key];
     }
     Track.find().sort({ numFavorite: -1 })
-        .limit(20).then(test => {
-            if (test.length == 0) {
-                res.status(200).json({
-                    Info: "result not founded"
-                })
-            }
-            else {
-                res.status(200).json(
-                    test
-                )
-            }
-        })
+    .limit(20).then(test=>{
+        if(test.length==0){
+            res.status(200).json({
+                Info: "result not founded"
+            })
+        }
+        else{
+        console.log(test)
+        res.status(200).json(    
+            test
+        )}
+})
 })
 router.get('/single_song', function (req, res) {
     var data
@@ -59,6 +59,7 @@ router.get('/single_song', function (req, res) {
             })
         }
         else {
+            console.log(test)
             res.status(200).json(
 
                 test[0]
@@ -68,13 +69,13 @@ router.get('/single_song', function (req, res) {
 })
 router.get('/search', function (req, res) {
     // console.log("req.query:",req.query.searchKey)
-    console.log("req.query:", req.query)
+    console.log("req.query:",req.query)
     var data
     for (var key in req.query) {
         data = req.query[key];
     }
     const regex = new RegExp(data, 'i')
-    Track.find({ 'title': { $regex: regex } }).then(test => {
+    Track.find({ 'title': { $regex: regex} }).then(test => {
 
         if (test.length == 0) {
             res.status(200).json({
@@ -82,36 +83,40 @@ router.get('/search', function (req, res) {
             })
         }
         else {
+            console.log(test)
             res.status(200).json(
+
                 test
             )
         }
     })
 })
-router.get('/song_file', function (req, res) {
-    let fileName = req.query.song
+router.get('/song_file',function(req,res){
+    let fileName=req.query.song
     const __dirname = path.resolve();
-    let options = { root: path.join(__dirname, './uploads/music') }
+    let options={ root: path.join(__dirname, './uploads/music') }
     res.set('Content-Type', 'audio/mpeg');
-    res.sendFile(fileName, options, function (err) {
+	res.sendFile(fileName, options, function (err) {
         if (err) {
-        } else {
+         } else {
             console.log('Sent:', fileName);
         }
     });
 })
-router.get('/photo_file', function (req, res) {
-    let fileName = req.query.photo
+router.get('/photo_file',function(req,res){
+    let fileName=req.query.photo
     const __dirname = path.resolve();
-    let options = { root: path.join(__dirname, './uploads/imageCover') }
+    let options={ root: path.join(__dirname, './uploads/imageCover') }
     res.sendFile(fileName, options, function (err) {
         if (err) {
+            console.log("err");
+            console.log(err);
         } else {
             console.log('Sent:', fileName);
         }
     });
 })
-router.post('/update_song', function (req, res) {
+router.post('/update_song',function(req,res){
     Update_song(req.body)
     console.log(req.body)
     res.send({
@@ -124,5 +129,4 @@ router.post('/create_song', function (req, res) {
         note: "save success!"
     })
 })
-
 export default router;
